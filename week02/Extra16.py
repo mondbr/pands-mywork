9a408b91eb7c886ad002d9e72beed43031a4bd29
MONIKA D <mondab133@gmail.com>
2024-02-07 22:37:39 +0000

    week02

---
+++ week02/Extra16.py
@@ -0,0 +1,9 @@
+#Extra16.py
+#this is an extra task in Lab 2.2
+#this program reads a name and age and outputs the following
+
+name = input ("Enter your name: ")
+age = input ("What is your age? ")
+print ("Hello " + name + "," + " your age is " + age + ".")
+
+
