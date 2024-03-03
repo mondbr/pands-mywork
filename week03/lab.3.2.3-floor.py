7651d38873e03fa71efef111bc78e6490bfe03e0
MONIKA D <mondab133@gmail.com>
2024-03-03 14:54:34 +0000

    week03

---
+++ week03/lab.3.2.3-floor.py
@@ -0,0 +1,20 @@
+# floor.py
+# this program takes a float and outputs an int rounded down
+
+# author: Monika Dabrowska
+
+
+
+
+import math
+
+number_to_floor = float(input("Enter a float number: "))
+
+# Use math.foor() to round down the float to the nearest integer
+floored_number = (math.floor(number_to_floor))
+
+
+# Prints the result
+print("{} floored is {}" .format((number_to_floor), floored_number))
+
+
