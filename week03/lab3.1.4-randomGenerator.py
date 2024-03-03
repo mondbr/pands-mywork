cb1f21ac4cad861a1fff8302f20f54d01fef5f61
MONIKA D <mondab133@gmail.com>
2024-03-03 13:33:52 +0000

    week03

---
+++ week03/lab3.1.4-randomGenerator.py
@@ -0,0 +1,7 @@
+# random.Generator.py
+# this program prints out a random number between 1 and 10
+
+import random
+
+number = random.randint(1,10)
+print ("here is a random number {}" .format (number))
