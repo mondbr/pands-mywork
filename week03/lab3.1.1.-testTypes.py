c7db6611efbc81759d3742b9328fc080af1c96dc
MONIKA D <mondab133@gmail.com>
2024-03-03 13:01:38 +0000

    week03

---
+++ week03/lab3.1.1.-testTypes.py
@@ -0,0 +1,21 @@
+# testTypes.py
+# this program shows 5 variables one of each of following types: 
+# - int
+# - float
+# - boolean
+# - str
+# - list
+
+i = 3
+fl = 3.5
+isa = True
+memo = "How on Brown Cow"
+lots = ["l", "o", "t", "s"]
+
+
+print ('variable {} is of type: {} and value: {}'.format('i', type (i), i))
+print ('variable {} is of type: {} and value: {}'.format('fl', type(fl), fl))
+print ('variable {} is of type: {} and value: {}'.format('isa', type(isa), isa))
+print ('variable {} is of type: {} and value: {}'.format('memo', type(memo), memo))
+print ('variable {} is of type: {} and value: {}'.format('lots', type(lots), lots))
+
