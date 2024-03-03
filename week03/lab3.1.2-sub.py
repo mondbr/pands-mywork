59d07bf46c99fc1ad502d78c456498bdd4e217ce
MONIKA D <mondab133@gmail.com>
2024-03-03 13:13:22 +0000

    week03

---
+++ week03/lab3.1.2-sub.py
@@ -0,0 +1,17 @@
+# sub.py
+
+# this program reads in two numbers and subtracts the first from the second one. 
+# input reads a string so we need to convert it into an int
+# so we can perform mathematical operations
+
+# author: Monika Dabrowska
+
+x = int(input("Enter your first number: "))
+y = int(input("Enter your second number: "))
+
+# if we enter a string or float, it will cause an error
+
+answer = x-y
+
+print("{} minus {} is {}".format(x, y, answer))
+
